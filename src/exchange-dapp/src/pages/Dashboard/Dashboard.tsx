f4715bd88842e509bbb03a73f827bac88db83e81
Vlad Nedelcu <nedelcuvd@gmail.com>
2022-10-31 17:13:42 +0200

    implemented exchange dapp

---
+++ src/exchange-dapp/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { useGetAccountInfo } from '@elrondnetwork/dapp-core/hooks';
+import axios from 'axios';
+import { EscrowService } from './escrow.service';
+
+export const Dashboard = () => {
+  const { address } = useGetAccountInfo();
+  const [escrowAddressRaw, setEscrowAddressRaw] = useState('');
+  const [escrowStatus, setEscrowStatus] = useState('');
+  const [balance, setBalance] = useState('');
+  const [fortune, setFortune] = useState('');
+  const [recordingOracleUrl, setRecordingOracleUrl] = useState('');
+
+  const setMainEscrow = async () => {
+    const escrowService = new EscrowService(escrowAddressRaw);
+
+    // Set escrow status
+    const escrowStatus = await escrowService.getStatus();
+    setEscrowStatus(escrowStatus);
+
+    // Set escrow balance
+    const escrowBalance = await escrowService.getBalance();
+    setBalance(escrowBalance.toString());
+
+    // Get manifest url
+    const { url: manifestUrl } = await escrowService.getManifest();
+    if (manifestUrl) {
+      const manifestContent = (await axios.get(manifestUrl)).data;
+      setRecordingOracleUrl(manifestContent.recording_oracle_url);
+    }
+  };
+
+  const sendFortune = async () => {
+    const payload = {
+      workerAddress: address,
+      escrowAddress: escrowAddressRaw,
+      fortune: fortune
+    };
+    console.log('red -> ', recordingOracleUrl);
+    try {
+      await axios.post(recordingOracleUrl, payload);
+      alert('Your fortune has been submitted');
+    } catch (err) {
+      alert('Could not submit fortune');
+    }
+    setFortune('');
+  };
+
+  return (
+    <div className='escrow-container'>
+      <div className='escrow-view'>
+        <div>
+          <input
+            onChange={(e) => setEscrowAddressRaw(e.target.value)}
+            value={escrowAddressRaw}
+          />
+          <button onClick={() => setMainEscrow()}> Confirm </button>
+        </div>
+        <span>
+          Fill the exchange address to pass the fortune to the recording oracle
+        </span>
+        <span>
+          <b>Address: </b> {escrowAddressRaw}
+        </span>
+        <span>
+          <b>Status: </b> {escrowStatus}
+        </span>
+        <span>
+          <b>Balance: </b> {balance}
+        </span>
+        <div>
+          <input onChange={(e) => setFortune(e.target.value)} />
+          <button onClick={sendFortune}> Send Fortune </button>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/exchange-dapp/src/pages/Dashboard/escrow.service.ts
@@ -0,0 +1,81 @@
+import { ProxyNetworkProvider } from '@elrondnetwork/erdjs-network-providers/out';
+import {
+  AbiRegistry,
+  Address,
+  Interaction,
+  ResultsParser,
+  SmartContract,
+  SmartContractAbi,
+  TypedOutcomeBundle
+} from '@elrondnetwork/erdjs/out';
+import escrowAbi from 'abi/job.abi.json';
+import { proxyNetwork } from '../../config';
+
+const abiRegistry = AbiRegistry.create(escrowAbi);
+const abi = new SmartContractAbi(abiRegistry);
+const networkProxy: string =
+  proxyNetwork || 'https://devnet-gateway.elrond.com';
+
+type UrlHashPair = {
+  url: string;
+  hash: string;
+};
+
+export class EscrowService {
+  contract: SmartContract;
+  networkProvider: ProxyNetworkProvider;
+
+  constructor(address: string) {
+    const contractAdress = new Address(address);
+    this.contract = new SmartContract({
+      address: contractAdress,
+      abi
+    });
+    this.networkProvider = new ProxyNetworkProvider(networkProxy);
+  }
+
+  async getStatus(): Promise<string> {
+    const interaction = <Interaction>this.contract.methods.getStatus();
+    try {
+      const { firstValue } = await this.performQuery(interaction);
+
+      return firstValue?.valueOf()?.name;
+    } catch (err) {
+      console.log('Error performing query -> getStatus');
+      return '';
+    }
+  }
+
+  async getBalance(): Promise<number> {
+    const interaction = <Interaction>this.contract.methods.getBalance();
+    const response = await this.performQuery(interaction);
+    const firstValue = response.firstValue?.valueOf();
+
+    return parseInt(firstValue);
+  }
+
+  async getManifest(): Promise<UrlHashPair> {
+    const interaction = <Interaction>this.contract.methods.getManifest();
+
+    const { firstValue } = await this.performQuery(interaction);
+
+    return {
+      url: firstValue?.valueOf()?.fields[0].name,
+      hash: firstValue?.valueOf()?.fields[1].name
+    };
+  }
+
+  private async performQuery(
+    interaction: Interaction
+  ): Promise<TypedOutcomeBundle> {
+    const resultParser = new ResultsParser();
+    const queryResponse = await this.networkProvider.queryContract(
+      interaction.check().buildQuery()
+    );
+
+    return resultParser.parseQueryResponse(
+      queryResponse,
+      interaction.getEndpoint()
+    );
+  }
+}
